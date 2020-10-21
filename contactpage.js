cd517941128f8671bc6822994de1a52820888d4d
JustinBushfield74474 <71091178+JustinBushfield74474@users.noreply.github.com>
2020-10-20 20:35:41 -0400

    CSS and JS updates to contact page, a small edit to syleguide

--- contactpage.js
+++ contactpage.js
@@ -1,16 +1,4 @@
 function sendEmail() {
-    var f = "a";
-    var q = "b";
-    var t = "c";
-    if (document.getElementById('F').value == 'on'){
-        f = "Feedback";
-    }
-    if (document.getElementById('Q').value == 'on'){
-        q = "Questions";
-    }
-    if (document.getElementById('T').value == 'on'){
-        t = "Typos";
-    }
     Email.send({
         Host: "smtp.gmail.com",
         Username: "tripito.info@gmail.com",
@@ -19,10 +7,12 @@ function sendEmail() {
         From: document.getElementById('email').value,
         Subject: 'Tripito Contact Form',
         Body: document.getElementById("name").value +
-            " has question(s) about " + f + ", " + q + ", " + t + ". Respond to them at: " +
-            document.getElementById("email").value,
+            " has question(s) about " + document.getElementById('Subject') + ". Respond to them at: " +
+            document.getElementById("email").value + " Body: " + document.getElementById("comments").value,
     })
-    .then(function (message) {
-        alert("Mail sent successfully!")
-    });
+    .then(
+        function (message) {
+            alert("Mail sent successfully!")
+        }
+    );
 }
